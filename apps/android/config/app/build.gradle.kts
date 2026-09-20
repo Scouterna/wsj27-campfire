f3f96294ce15439933e60879a4fcd18071661a0d
Joakim Kemeny <joakim@kemeny.se>
2026-09-20 20:19:05 +0200

    chore: raise the version to 2026.8.0

--- apps/android/config/app/build.gradle.kts
+++ apps/android/config/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     minSdk = 30
     targetSdk = 36
     versionCode = 1
-    versionName = "2026.7.2"
+    versionName = "2026.8.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
